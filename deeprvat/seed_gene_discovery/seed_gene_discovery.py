4c850b07feaa3e6f84cb684db1fe6fab797fd83e
Magnus Wahlberg <endast@gmail.com>
2023-08-04 10:09:31 +0200

    Cleanup seed gene discovery (#13)
    * Update annotations.parquet
    
    add is_plof column
    
    * Change combined_UKB_NFE_AF to MAF
    
    * Update seed_gene_discovery.py
    
    Remove unused imports
    
    * Rename genes.parquet to protein_coding_genes.parquet
    
    * Remove unused imports
    
    * Add plotnine dep
    
    * Downgrade plotnine
    
    * Update github-actions.yml
    
    Add Run seed_gene_discovery pipeline
    
    * Revert removal of unused imports
    
    * Remove unused imports and commented out code
    
    * Blackify
    
    * Black format

--- deeprvat/seed_gene_discovery/seed_gene_discovery.py
+++ deeprvat/seed_gene_discovery/seed_gene_discovery.py
@@ -9,14 +9,11 @@ from pathlib import Path
 from typing import Any, Dict, List, Optional, Tuple
 
 import click
-import dask.dataframe as dd
 import numpy as np
 import pandas as pd
 import yaml
-import random
 from scipy.stats import beta
 from scipy.sparse import coo_matrix, spmatrix
-from statsmodels.stats.multitest import fdrcorrection
 from torch.utils.data import DataLoader, Dataset
 from tqdm import tqdm
 
@@ -169,9 +166,6 @@ def get_anno(
     var_weight_function: str,
     maf_col: str,
 ):
-
-    # temp_genotypes -= np.nanmean(temp_genotypes, axis=0)
-    # G1 = np.ma.masked_invalid(temp_genotypes).filled(0.)
     assert np.all(np.isfinite(G))
 
     weights, plof_mask = get_weights(
@@ -191,12 +185,12 @@ def call_score(GV, null_model_score, pval_dict, test_type):
             f"Negative value encountered in p-value computation "
             f"p-value: {pv}, using saddle instead."
         )
-        #get test time
+        # get test time
         start_time = time.time()
         pv = null_model_score.pv_alt_model(GV, method="saddle")
         end_time = time.time()
         time_diff = end_time - start_time
-        pval_dict['time'] = time_diff
+        pval_dict["time"] = time_diff
     pval_dict["pval"] = pv
     if pv < 1e-3 and test_type == "burden":
         logger.info("Computing regression coefficient")
@@ -206,6 +200,7 @@ def call_score(GV, null_model_score, pval_dict, test_type):
         pval_dict["beta"] = beta["beta"][0, 0]
         pval_dict["betaSd"] = np.sqrt(beta["var_beta"][0, 0])
     return pval_dict
+
 
 # set up the test-function for a single gene
 def test_gene(
@@ -233,7 +228,7 @@ def test_gene(
     # Important: cast G into numpy array. Otherwise it will be a matrix and
     # the * operator does matrix mutiplication (.dot()) instead of scalar multiplication (.multiply())
     G = np.asarray(G)
-    logger.info(f'G shape and sum {G.shape, G.sum()}')
+    logger.info(f"G shape and sum {G.shape, G.sum()}")
     # GET expected allele count (EAC) as in Karczewski et al. 2022/Genebass
     vars_per_sample = np.sum(G, axis=1)
     samples_with_variant = vars_per_sample[vars_per_sample > 0].shape[0]
@@ -257,7 +252,10 @@ def test_gene(
 
     # keep backwards compatibility
 
-    weights, _, = get_anno(
+    (
+        weights,
+        _,
+    ) = get_anno(
         G, variant_ids, annotation_df, weight_cols, var_weight_function, maf_col
     )
     variant_weight_th = (
@@ -320,7 +318,7 @@ def test_gene(
     else:
         GW = GW_full = np.zeros(G.shape[0])
 
-    return pval_dict, GW, GW_full  
+    return pval_dict, GW, GW_full
 
 
 def run_association_(
@@ -348,7 +346,9 @@ def run_association_(
     # Get column with minor allele frequency
     annotations = config["data"]["dataset_config"]["annotations"]
     maf_col = [
-        annotation for annotation in annotations if re.search(r"_AF|_MAF|^MAF", annotation)
+        annotation
+        for annotation in annotations
+        if re.search(r"_AF|_MAF|^MAF", annotation)
     ]
     assert len(maf_col) == 1
     maf_col = maf_col[0]
@@ -366,7 +366,7 @@ def run_association_(
                 config["test_config"],
                 var_type,
                 test_type,
-                maf_col
+                maf_col,
             )
             if persist_burdens:
                 GW_list[gene] = GW
@@ -629,7 +629,6 @@ def run_association(
     if isinstance(G_full, spmatrix):
         G_full = G_full.tocsr()
     G_full = G_full[this_data_idx]
-    ## HAKIME
     logger.info(f"all_variants shape: {all_variants.shape}")
 
     X = data_full["x_phenotypes"].numpy()[this_data_idx]
@@ -657,7 +656,6 @@ def run_association(
 
     logger.info(f"Number of genes to test: {len(gene_ids)}")
     # grouped annotations also contains non-coding gene ids
-    # gene_ids = list(grouped_annotations.groups.keys())
 
     if debug:
         logger.info("Debug mode: Using only 100 genes")
@@ -734,8 +732,6 @@ def combine_results(result_files: Tuple[str], out_file: str):
     logger.info("Doing simple postprocessing of results")
 
     cols_to_keep = ["gene", "EAC", "pval"]
-
-    # res_df.to_parquet(out_file)
 
     if "EAC" in res_df.columns:
         logger.info("Filtering for genes with EAC > 50")
