1e5b824f2e439f5d23259d25b2b21315ae16d6ab
Magnus Wahlberg <endast@gmail.com>
2024-04-12 12:13:35 +0200

    Create test skeleton for annotations

---
+++ tests/annotations/test_annotations.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+
+import pandas as pd
+import pytest
+from click.testing import CliRunner
+
+from deeprvat.annotations.annotations import cli as annotations_cli
+
+script_dir = Path(__file__).resolve().parent
+tests_data_dir = script_dir / "test_data"
+
+
+@pytest.mark.parametrize(
+    "test_data_name_dir, in_variants, out_variants, expected_out_variants",
+    [
+        (
+            "process_annotations_small",
+            "in_variants.parquet",
+            "out_variants.parquet",
+            "expected_variants.parquet",
+        ),
+    ],
+)
+def test_process_annotations(
+    test_data_name_dir, in_variants, out_variants, expected_out_variants, tmp_path
+):
+    cli_runner = CliRunner()
+
+    current_test_data_dir = tests_data_dir / "process_annotations" / test_data_name_dir
+
+    in_variants_file = current_test_data_dir / in_variants
+    out_variants_file = tmp_path / out_variants
+    expected_out_variants_file = current_test_data_dir / expected_out_variants
+
+    norm_variants_dir = tmp_path / "norm/variants"
+    qc_duplicate_vars_dir = tmp_path / "qc/duplicate_vars"
+    norm_variants_dir.mkdir(parents=True, exist_ok=True)
+    qc_duplicate_vars_dir.mkdir(parents=True, exist_ok=True)
+
+    cli_parameters = [
+        "process-annotations",
+        in_variants_file.as_posix(),
+        out_variants_file.as_posix(),
+    ]
+
+    result = cli_runner.invoke(annotations_cli, cli_parameters)
+    assert result.exit_code == 0
+
+    written_results = pd.read_parquet(out_variants_file)
+
+    expected_variants_data = pd.read_parquet(expected_out_variants_file)
+
+    assert expected_variants_data == written_results
