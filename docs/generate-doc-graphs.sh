7fb45806f7dca2c2de53491b4466b37737314fa0
Magnus Wahlberg <endast@gmail.com>
2024-04-16 14:52:04 +0200

    Update generate-doc-graphs.sh

--- docs/generate-doc-graphs.sh
+++ docs/generate-doc-graphs.sh
@@ -2,43 +2,54 @@
 
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-
-generate_rule_graph() {
-  SNAKEFILE="$1"
-  DIRECTORY="$2"
-  CONFIG="$3"
-  OUTPUT_FILE="$4"
-
-  echo "Generating rule graph: $OUTPUT_FILE"
-
-
-  snakemake -n --snakefile "$SNAKEFILE" --directory "$DIRECTORY" --configfile "$CONFIG" --forceall --rulegraph \
-    | tail -n +2  | awk '!/color=/{gsub(/color = "[^"]+",/, "");} {gsub(/,$/, "");} 1' | awk '{$1=$1}1' \
-    | dot -Tsvg > "$OUTPUT_FILE"
-}
-
-
-generate_dag_graph() {
-  SNAKEFILE="$1"
-  DIRECTORY="$2"
-  CONFIG="$3"
-  OUTPUT_FILE="$4"
-  echo "Generating dag graph: $OUTPUT_FILE"
-  snakemake -n --snakefile "$SNAKEFILE" --directory "$DIRECTORY" --configfile "$CONFIG" --forceall --dag \
-    | tail -n +2 | dot -Tsvg > "$OUTPUT_FILE"
-}
-
-
-
-
 PIPELINE_DIR="$SCRIPT_PATH/../pipelines"
 CONFIG_DIR="$PIPELINE_DIR/config"
 WORK_DIR="$SCRIPT_PATH/../example"
 STATIC_DIR="$SCRIPT_PATH/_static"
 
-generate_rule_graph "$PIPELINE_DIR/preprocess_with_qc.snakefile" "$WORK_DIR/preprocess" "$CONFIG_DIR/deeprvat_preprocess_config.yaml" "$STATIC_DIR/preprocess_rulegraph_with_qc.svg"
-generate_rule_graph "$PIPELINE_DIR/preprocess_no_qc.snakefile" "$WORK_DIR/preprocess" "$CONFIG_DIR/deeprvat_preprocess_config.yaml" "$STATIC_DIR/preprocess_rulegraph_no_qc.svg"
 
-generate_rule_graph "$PIPELINE_DIR/annotations.snakefile"  "$WORK_DIR/annotations" "$CONFIG_DIR/deeprvat_annotation_config.yaml" "$STATIC_DIR/annotation_rulegraph.svg"
-generate_dag_graph "$PIPELINE_DIR/annotations.snakefile"  "$WORK_DIR/annotations" "$CONFIG_DIR/deeprvat_annotation_config.yaml" "$STATIC_DIR/annotation_pipeline_dag.svg"
+generate_rule_graph() {
+  SNAKEFILE="$PIPELINE_DIR/$1"
+  DIRECTORY="$2"
+  CONFIG="$3"
+  OUTPUT_FILE="$STATIC_DIR/$(basename $SNAKEFILE |cut -f 1 -d'.')_rulegraph.svg"
 
+  echo "Generating rule graph: $OUTPUT_FILE"
+
+  snakemake -n --snakefile "$SNAKEFILE" --directory "$DIRECTORY" --configfile "$CONFIG" --forceall --rulegraph \
+    | sed -n '/digraph/,$p' | awk '!/color=/{gsub(/color = "[^"]+",/, "");} {gsub(/,$/, "");} 1' | awk '{$1=$1}1' \
+    | dot -Tsvg > "$OUTPUT_FILE"
+
+  echo -e "---------\n"
+}
+
+
+generate_dag_graph() {
+  SNAKEFILE="$PIPELINE_DIR/$1"
+  DIRECTORY="$2"
+  CONFIG="$3"
+  OUTPUT_FILE="$STATIC_DIR/$(basename $SNAKEFILE |cut -f 1 -d'.')_dag.svg"
+
+  echo "Generating dag graph: $OUTPUT_FILE"
+  snakemake -n --snakefile "$SNAKEFILE" --directory "$DIRECTORY" --configfile "$CONFIG" --forceall --dag \
+    | sed -n '/digraph/,$p' | dot -Tsvg > "$OUTPUT_FILE"
+}
+
+
+generate_rule_graph "preprocess_with_qc.snakefile" "$WORK_DIR/preprocess" "$CONFIG_DIR/deeprvat_preprocess_config.yaml"
+generate_rule_graph "preprocess_no_qc.snakefile" "$WORK_DIR/preprocess" "$CONFIG_DIR/deeprvat_preprocess_config.yaml"
+
+generate_rule_graph "annotations.snakefile"  "$WORK_DIR/annotations" "$CONFIG_DIR/deeprvat_annotation_config.yaml"
+generate_dag_graph "annotations.snakefile"  "$WORK_DIR/annotations" "$CONFIG_DIR/deeprvat_annotation_config.yaml"
+
+generate_rule_graph "association_testing_pretrained.snakefile" "$WORK_DIR" "$WORK_DIR/config.yaml"
+
+generate_rule_graph "association_testing_pretrained_regenie.snakefile" "$WORK_DIR" "$WORK_DIR/config.yaml"
+
+generate_rule_graph "run_training.snakefile" "$WORK_DIR" "$WORK_DIR/config.yaml"
+
+generate_rule_graph "seed_gene_discovery.snakefile" "$WORK_DIR" "$WORK_DIR/config.yaml"
+
+generate_rule_graph "training_association_testing.snakefile" "$WORK_DIR" "$WORK_DIR/config.yaml"
+
+generate_rule_graph "training_association_testing_regenie.snakefile" "$WORK_DIR" "$WORK_DIR/config.yaml"
