ebe5584385e8d8928b6bb8cdd2acd6b6115f87f0
Mück <m991k@b260-pc003.inet.dkfz-heidelberg.de>
2024-04-26 16:12:31 +0200

    added concat annotations test

--- tests/annotations/test_annotations.py
+++ tests/annotations/test_annotations.py
@@ -221,3 +221,35 @@ def test_merge_annotations(
     expected_results = pd.read_parquet(expected_path)
     assert written_results.shape == expected_results.shape
     assert_frame_equal(written_results[expected_results.columns], expected_results, check_exact = False)
+
+
+@pytest.mark.parametrize(
+    "test_data_name_dir, expected, merged_files",
+    [
+        (
+            "concatenate_annotations_small",
+            "expected.parquet",
+            "chr3test_merged.parquet,chr4test_merged.parquet",
+        ),
+    ]
+)
+def test_concatenate_annotations(
+    test_data_name_dir, expected, merged_files, tmp_path
+    ):
+    current_test_data_dir = tests_data_dir / 'concatenate_annotations'/ test_data_name_dir
+    expected_path = current_test_data_dir / 'expected' / expected
+    merged_files  = [(current_test_data_dir/'input'/ file).as_posix() for file in merged_files.split(',')]
+    output_path = tmp_path / 'out.parquet'
+    cli_runner = CliRunner()
+
+    cli_parameters = ["concat-annotations",
+                      ",".join(merged_files),
+                      output_path.as_posix(),
+                      ]
+    
+    result = cli_runner.invoke(annotations_cli, cli_parameters, catch_exceptions=False)
+    assert result.exit_code == 0
+    written_results = pd.read_parquet(output_path)
+    expected_results = pd.read_parquet(expected_path)
+    assert written_results.shape == expected_results.shape
+    assert_frame_equal(written_results[expected_results.columns], expected_results, check_exact = False)
