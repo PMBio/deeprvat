d114b9a051c81d8932fedd0022351209e8b89fff
PMBio <PMBio@users.noreply.github.com>
2024-05-29 07:03:54 +0000

    fixup! Format Python code with psf/black pull_request

--- deeprvat/deeprvat/config.py
+++ deeprvat/deeprvat/config.py
@@ -205,7 +205,8 @@ def create_main_config(
             "config"
         ]["thresholds"][k] = f"{k} {v}"
     # Results evaluation parameters; alpha parameter for significance threshold
-    if "evaluation" not in full_config: full_config["evaluation"] = {}
+    if "evaluation" not in full_config:
+        full_config["evaluation"] = {}
     full_config["evaluation"]["correction_method"] = input_config["evaluation"][
         "correction_method"
     ]
@@ -226,7 +227,8 @@ def create_main_config(
         # Training Phenotypes
         full_config["training"]["phenotypes"] = input_config["phenotypes_for_training"]
         # Baseline results
-        if ["baseline_results"] not in full_config: full_config["baseline_results"] = {}
+        if ["baseline_results"] not in full_config:
+            full_config["baseline_results"] = {}
         full_config["baseline_results"]["options"] = input_config["seed_gene_results"][
             "options"
         ]
