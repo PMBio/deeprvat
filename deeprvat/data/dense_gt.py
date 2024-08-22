90c65a7af7062e7805b76fc76cf7c1d4b2d0aaee
Brian Clarke <brian.clarke@dkfz.de>
2024-08-22 16:03:19 +0200

    make genotype and variant files optional

--- deeprvat/data/dense_gt.py
+++ deeprvat/data/dense_gt.py
@@ -112,7 +112,7 @@ class DenseGTDataset(Dataset):
         else:
             logger.setLevel(logging.INFO)
 
-        self.check_samples = False # NOTE: Set to True for debugging
+        self.check_samples = False  # NOTE: Set to True for debugging
         self.split = split
         self.train_dataset = train_dataset
         self.chromosomes = (
