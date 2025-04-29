f2872217fa07c24534a5f94b154cd7ed86910474
PMBio <PMBio@users.noreply.github.com>
2025-04-29 08:10:25 +0000

    fixup! Format Python code with psf/black pull_request

--- tests/annotations/test_annotations.py
+++ tests/annotations/test_annotations.py
@@ -906,9 +906,9 @@ def test_aggregate_absplice_manually(
     expected_results = pd.read_parquet(expected_path)
     assert written_results.shape == expected_results.shape
     assert_frame_equal(
-        written_results.sort_values(by=["chrom", "pos", "ref", "alt", "gene_id"]).reset_index(
-            drop=True
-        ),
+        written_results.sort_values(
+            by=["chrom", "pos", "ref", "alt", "gene_id"]
+        ).reset_index(drop=True),
         expected_results[written_results.columns]
         .sort_values(by=["chrom", "pos", "ref", "alt", "gene_id"])
         .reset_index(drop=True),
